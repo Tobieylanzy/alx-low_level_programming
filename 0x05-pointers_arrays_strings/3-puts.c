ed8729f7b17f57d53c8d6b1239d33e01d076f82a
Tobieylanzy <Agbeoluwatobi@gmail.com>
2023-06-26 09:58:39 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,7 @@
+
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line,
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
 void _puts(char *str)
@@ -10,4 +11,5 @@ void _puts(char *str)
 		_putchar(*str++);
 	}
 		_putchar('\n');
+}
 
